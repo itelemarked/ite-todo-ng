87a677944fa8199d4a7156eeb0df8e5e98fc1aa3
itelemarked home <itelemarked@icloud.com>
2026-03-12 20:49:16 +0100

    clean-up app files

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [],
+  template: `
+    App works!
+  `,
+  styles: [``]
 })
 export class App {
-  protected readonly title = signal('ite-todo-ng');
+  
 }
